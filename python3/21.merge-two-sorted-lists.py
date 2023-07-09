5de82c80f96cee5ffba8ef20b685c090cb0e2547
K-Tatsuya514 <kobatatsu0514@gmail.com>
2023-07-09 23:57:06 +0900

    modified 21 add comment

--- python3/21.merge-two-sorted-lists.py
+++ python3/21.merge-two-sorted-lists.py
@@ -16,19 +16,26 @@
 # Your runtime beats 15.61 % of python3 submissions
 # Your memory usage beats 14.53 % of python3 submissions (16.5 MB)
 
+# vscodeで*** is not definedを回避する
 from typing import Optional
 from typing import List
 
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # Linked list を配列arrayに変形する
         array_list1: List = self.__get_list(list1)
         array_list2: List = self.__get_list(list2)
 
+        # 並列に変更後、list1とlist2を結合する
         array_list3 = array_list1 + array_list2
 
+        # 戻り値のlist3
         list3 = None
+
+        # 配列のsortedを利用してソート後、linked listの形に戻す
         for c in sorted(array_list3):
+            # list3がNone=はじめの場合
             if list3 is None:
                 list3 = ListNode(c)
             else:
@@ -39,6 +46,7 @@ class Solution:
 
         return list3
 
+    # Linked list を配列arrayに変形する
     def __get_list(self, list_item):
         current = list_item
         array_list = []
